338a34f1d3164570b5bcc354274024594505a458
thecodegoblin <codegoblindev@gmail.com>
2019-11-29 00:28:00 +0000

    add log

--- app/methods/methods.js
+++ app/methods/methods.js
@@ -4,6 +4,7 @@ var User = require('./../models/user');
 var methods = {
        
 	createNewUser : function(username) {
+        console.log("creating new user...");
 		var account = new User();
 		account.username = username;
 		return account.saveAsync();
